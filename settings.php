2db965b1482fd09af0bf140543fd2ed176c917d6
Daniel Moreira <51049268+mnemsypemsy@users.noreply.github.com>
2023-12-01 18:55:10 +0100

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * These paths are used for making API requests to different endpoints.
+ * 
+
+ * Aug 2023
+ */
+
+$sURL = "https://aap/";
+
+$apiPathLogin = 'api/login/';
+$apiPathUsers =  'api/v2/users/';
+$apiPathPesonalTokens = '/personal_tokens/';
+$apiPathJobTemplates = 'api/v2/job_templates/';
+$apiPathSurvey = '/survey_spec/';
+$apiPathLaunch = '/launch/';
+?>
