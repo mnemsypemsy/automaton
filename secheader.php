140124de118531de426ca792e0cf2c850f53741d
Daniel Moreira <51049268+mnemsypemsy@users.noreply.github.com>
2023-12-01 18:55:44 +0100

    Create secheader.php

---
+++ secheader.php
@@ -0,0 +1,6 @@
+<?php
+ini_set('session.cookie_secure', 1);
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+?>
